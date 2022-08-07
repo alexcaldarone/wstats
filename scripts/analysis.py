c228b47286d673a483ec5cab3b95b5c77e29ae0a
Alex Caldarone <71179853+alexcaldarone@users.noreply.github.com>
2022-08-07 14:58:26 +0200

    fix message type dictionaty

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -72,6 +72,7 @@ class Analysis:
         self["Days"].clear()
         self["Number"].clear()
         self["NumberWords"].clear()
+        self["Type] = dict.fromkeys(self["Type"], 0) # resets all the values of the type dictionay to zero
         self.last_date = None # attribute used to determine who started a conversation
 
     def update_stats(self, message: Message): # make all the dictionary checks the same
