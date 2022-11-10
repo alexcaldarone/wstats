a95642ff8b559fddbff7138eb9ba5008edaf7500
Alex <cldrna_coding.is235@aleeas.com>
2022-11-10 22:05:25 +0100

    Added test for date attribute (Message class)

--- tests/test_message.py
+++ tests/test_message.py
@@ -1,18 +1,46 @@
 import unittest
+from datetime import datetime
 from scripts.message import Message
 
+'''
+To add:
+    - Test creation of date attribute
+    - test time attribute
+    - test author
+    - test content
+    - test type
+    - test weekday
+'''
 
 class TestMessage(unittest.TestCase):
 
-    def test_message(self):
+    def test_valid_message(self):
         """
         Test against different input messages
         """
 
-        with open('tests/data/chat_test_1.txt') as f:
+        with open('tests/data/chat_test_1.txt', 'r') as f:
 
             for l in f:
 
                 message = Message(l)
 
                 self.assertEqual(message.is_valid_message(l), True)
+    
+    
+    def test_date(self):
+        """
+        Tests that the date attribute of the chat messages is created correctly
+        """
+        reference_date = datetime(year = 2022, month = 3, day = 29)
+
+        with open('tests/data/chat_test_1.txt', 'r') as f:
+
+            for l in f:
+                
+                message = Message(l)
+
+                self.assertEqual(message.date, reference_date)
+
+if __name__ == '__main__':
+    unittest.main()
